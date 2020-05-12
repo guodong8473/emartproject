107b4ee9271b753d128baed6dd836421a43706ab
jguoding <jguoding@cn.ibm.com>
2020-05-12 21:08:51 +0800

    commit

--- server/emart_springcloud/emart_item/src/test/java/org/emart_item/service/ItemServiceTest.java
+++ server/emart_springcloud/emart_item/src/test/java/org/emart_item/service/ItemServiceTest.java
@@ -1,4 +1,4 @@
-package org.emart_item;
+package org.emart_item.service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -7,17 +7,16 @@ import javax.persistence.Persistence;
 import org.emart_item.entity.ItemEntity;
 import org.junit.jupiter.api.Test;
 
+class ItemServiceTest {
 
-public class AppTest {
-
-	   @Test public void getPerson(){	   
+	@Test
+	void testGetItem() {
 		   EntityManagerFactory factory=Persistence.createEntityManagerFactory("emartHibernate");
 		   EntityManager em=factory.createEntityManager();
 		   ItemEntity item=em.find(ItemEntity.class,1);
-		  // em.refresh(person);//若直接更改了数据库中的数据，应该用refresh刷新一下数据库的动态变化，保证输出正确结果
 		   System.out.println(item);	   
 		   em.close();
-		   factory.close();	   	   
-	   }
+		   factory.close();	   	 
+	}
 
 }
